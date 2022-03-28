c96b2e910f1e3ff3a637a9905b1ac5881ac3d124
sunzhenliang <sunzhenliang@ebupt.com>
2022-03-28 14:27:12 +0800

    rm nvim setup

--- setup.sh
+++ setup.sh
@@ -5,22 +5,23 @@
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Install Python3
 sudo apt-get install -y python-dev python-pip python3-dev
 sudo apt-get install -y python3-setuptools
 
 # Install neovim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install -y neovim
+# sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo apt-get update
+# sudo apt-get install -y neovim
 
 # Install SpcaceVim in nvim
-curl -sLf https://spacevim.org/cn/install.sh | bash -s -- --install neovim
-git clone https://github.com/HiSunzhenliang/my-spacevim-conf.git ~/.SpcaceVim.d
+# curl -sLf https://spacevim.org/cn/install.sh | bash -s -- --install neovim
+# git clone https://github.com/HiSunzhenliang/my-spacevim-conf.git ~/.SpcaceVim.d
 
-# System-wise settings
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-
+# Sys
 cat ./alias >> ~/.zshrc
 
 # Create ssh key
