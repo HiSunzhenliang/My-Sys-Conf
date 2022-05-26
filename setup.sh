883053fec65d4eb9f23f579fb9bda9f07b6262ce
sunzhenliang <hisunzhenliang@outlook.com>
2022-05-26 11:22:50 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
 
-
-# Install zsh && ohmyzsh
 sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# powerlevel10k theme
+bash ohmyzsh-install.sh
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Install Python3
-sudo apt-get install -y python-dev python-pip python3-dev
-sudo apt-get install -y python3-setuptools
+cp ./.zshrc ~/.zshrc
 
-# Install neovim
-# sudo add-apt-repository ppa:neovim-ppa/stable
-# sudo apt-get update
-# sudo apt-get install -y neovim
+cp ./.vimrc ~
 
-# Install SpcaceVim in nvim
-# curl -sLf https://spacevim.org/cn/install.sh | bash -s -- --install neovim
-# git clone https://github.com/HiSunzhenliang/my-spacevim-conf.git ~/.SpcaceVim.d
-
-# Sys
-cat ./alias >> ~/.zshrc
-
-cat ./zshrc >> ~/.zshrc
+cp ./.vim ~ -r
 
 # Create ssh key
 ssh-keygen -t rsa
