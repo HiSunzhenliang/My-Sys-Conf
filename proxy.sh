072d4fdf9ea0d5f6d73d68b5a2c17b38c8a84386
sunzhenliang <hisunzhenliang@outlook.com>
2022-10-24 15:10:14 +0800

    add proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
 export https_proxy=http://$host_ip:33210 http_proxy=http://$host_ip:33210 all_proxy=socks5://$host_ip:33211
+echo $https_proxy $http_proxy $all_proxy
